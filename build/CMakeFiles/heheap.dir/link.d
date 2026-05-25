302e5dff8e863ec0f2b3b9297402cb99c9bfa1c5
movr7sp <movr7sp@gmail.com>
2026-05-25 14:24:15 +0300

    init on linux

---
+++ build/CMakeFiles/heheap.dir/link.d
@@ -0,0 +1,127 @@
+heheap: \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/Scrt1.o \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/crti.o \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/crtbeginS.o \
+  CMakeFiles/heheap.dir/src/main.cpp.o \
+  lib/glfw/src/libglfw3.a \
+  libimgui.a \
+  /usr/lib/libGLX.so \
+  /usr/lib/libOpenGL.so \
+  /usr/lib/librt.a \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libdl.a \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libstdc++.so \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libm.so \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libm.so \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libm.so \
+  /usr/lib64/libm.so.6 \
+  /usr/lib64/libmvec.so.1 \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so.1 \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libc.so \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libc.so \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libc.so \
+  /usr/lib64/libc.so.6 \
+  /usr/lib64/libc_nonshared.a \
+  /usr/lib64/ld-linux-x86-64.so.2 \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so.1 \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/libgcc.a \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/crtendS.o \
+  /usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/crtn.o \
+  /lib64/libGLdispatch.so.0 \
+  /lib64/libX11.so.6 \
+  /usr/lib64/ld-linux-x86-64.so.2 \
+  /lib64/libxcb.so.1 \
+  /lib64/libXau.so.6 \
+  /lib64/libXdmcp.so.6
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/Scrt1.o:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/crti.o:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/crtbeginS.o:
+
+CMakeFiles/heheap.dir/src/main.cpp.o:
+
+lib/glfw/src/libglfw3.a:
+
+libimgui.a:
+
+/usr/lib/libGLX.so:
+
+/usr/lib/libOpenGL.so:
+
+/usr/lib/librt.a:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libdl.a:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libstdc++.so:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libm.so:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libm.so:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libm.so:
+
+/usr/lib64/libm.so.6:
+
+/usr/lib64/libmvec.so.1:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so.1:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libc.so:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libc.so:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libc.so:
+
+/usr/lib64/libc.so.6:
+
+/usr/lib64/libc_nonshared.a:
+
+/usr/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so.1:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/crtendS.o:
+
+/usr/lib64/gcc/x86_64-unknown-linux-gnu/14.2.1/../../../../lib64/crtn.o:
+
+/lib64/libGLdispatch.so.0:
+
+/lib64/libX11.so.6:
+
+/usr/lib64/ld-linux-x86-64.so.2:
+
+/lib64/libxcb.so.1:
+
+/lib64/libXau.so.6:
+
+/lib64/libXdmcp.so.6:
